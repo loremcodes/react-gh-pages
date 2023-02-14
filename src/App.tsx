02cd9618d719d57052d8f6e9873c000638a76986
loremcodes <prasadjunghare2@gmail.com>
2023-02-14 14:19:56 +0530

    Final Test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     {/*  <header className="App-header">
+      {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,9 +19,27 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <h1>
-        Hello world from gh-pages
-      </h1>
+
+      {/* Header */}
+      <header className='App-header'>
+        <h1>Prasad Junghare</h1>
+        <a className='App-button' href="#">Resume</a>
+      </header>
+      {/* Intro section */}
+      <div className='App-introduction'>
+        <h2>Prasad Junghare</h2>
+        <h3>Frontend Developer</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quasi nemo? Fuga modi expedita dolorum accusamus, eligendi fugiat ea labore.</p>
+      </div>
+      {/* Socail Icons */}
+      <div className='App-icons'>
+        <a target="_blank"
+          rel="noopener noreferrer" className='App-link' href="https://github.com/loremcodes">Github</a>
+        <a target="_blank"
+          rel="noopener noreferrer" className='App-link' href="https://insatgram.com/thejunghare">Instagram</a>
+        <a target="_blank"
+          rel="noopener noreferrer" className='App-link' href="#">LinkedIn</a>
+      </div>
     </div>
   );
 }
